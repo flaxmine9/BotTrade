cd2d3857ffa38328b52a7586abe662535051a67e
flaxmine9 <flaxmine9@gmail.com>
2021-10-03 14:27:54 +0300

    added a new files -- maxOrders

--- TradeBinance/TradeSetting.cs
+++ TradeBinance/TradeSetting.cs
@@ -5,18 +5,20 @@
         public decimal TakeProfit { get; set; }
         public decimal StopLoss { get; set; }
         public int Leverage { get; set; }
+        public int MaxOrders { get; set; }
 
         /// <summary>
         /// Types: Isolated, Cross
         /// </summary>
         public string FuturesMarginType { get; set; }
 
-        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType)
+        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders)
         {
             TakeProfit = takeProfit;
             StopLoss = stopLoss;
             Leverage = leverage;
             FuturesMarginType = futuresMarginType;
+            MaxOrders = maxOrders;
         }
     }
 }
