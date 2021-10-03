2a33bfb98dd5e576b255fc48b541340fc64aa50b
flaxmine9 <flaxmine9@gmail.com>
2021-10-03 14:28:10 +0300

    edited

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -1,5 +1,7 @@
-﻿using Strategy;
+﻿using Strategies;
+using Strategy;
 using System;
+using System.Collections.Generic;
 using TechnicalIndicator.Pivot;
 using TechnicalIndicator.Pivot.PivotTypes;
 using TradeBinance;
@@ -10,35 +12,34 @@ namespace FlaxTrade
     {
         public static void Main(string[] args)
         {
-            //string key = "Bam9fBnYh5ZKES83ZylyHAs3nekWi22pODywKiYjRlXhTo38XQcaJk2HzrVZJPiU";
-            //string secretKey = "6F2LYFGc6cgLnn5fZ3gzD2ydI0xrAKA3kpvxVjOaMYXMKtN2ukk1p4TlI2NpB8QR";
+            string key = "Bam9fBnYh5ZKES83ZylyHAs3nekWi22pODywKiYjRlXhTo38XQcaJk2HzrVZJPiU";
+            string secretKey = "6F2LYFGc6cgLnn5fZ3gzD2ydI0xrAKA3kpvxVjOaMYXMKtN2ukk1p4TlI2NpB8QR";
 
-            string keyTest = "581d213f387a76f1280c9072d6df0112d519dbd05752e3175feb60b80726c0a5";
-            string secretKeyTest = "69ad2ca09dfb9f21a325e1430feb5b423c763a8aa497071ca341b01adf693df2";
-            
+            //string keyTest = "581d213f387a76f1280c9072d6df0112d519dbd05752e3175feb60b80726c0a5";
+            //string secretKeyTest = "69ad2ca09dfb9f21a325e1430feb5b423c763a8aa497071ca341b01adf693df2";
+
             //string keyTestKirill = "2929b75ae210c87f385e2bf0bd8f62bfa4a16da303272fb9bd765876a4a484a7";
             //string secretKeyTestKirill = "243ed78641c70fd7a7336a05d26cde385ec8f1d3ce1320aa8bfd1bbe4bb96f7b";
 
 
-            Client client = new Client(new ApiSetting() { Key = keyTest, SecretKey = secretKeyTest });
+            Client client = new Client(new ApiSetting() { Key = key, SecretKey = secretKey });
             client.AddStrategy(new StrategyCPR
                 (
-                   new TradeSetting(takeProfit: 1.05m, stopLoss: 1.025m, leverage: 5, futuresMarginType: "Isolated"),
+                   new TradeSetting(takeProfit: 1.02m, stopLoss: 1.015m, leverage: 5, futuresMarginType: "Isolated", maxOrders: 5),
                    new PivotPoint(new Traditional())
 
                 ));
 
             client.StartStrategies();
 
-            //Client clientKirill = new Client(new ApiSetting() { Key = keyTestKirill, SecretKey = secretKeyTestKirill });
-            //clientKirill.AddStrategy(new StrategyCPR
-            //    (
-            //       new TradeSetting(takeProfit: 1.05m, stopLoss: 1.025m, leverage: 5, futuresMarginType: "Isolated"),
-            //       new PivotPoint(new Traditional())
 
+            //Client client = new Client(new ApiSetting() { Key = key, SecretKey = secretKey });
+            //client.AddStrategy(new SuperTrendHeikin
+            //    (
+            //       new TradeSetting(takeProfit: 1.02m, stopLoss: 1.015m, leverage: 5, futuresMarginType: "Isolated")
             //    ));
 
-            //clientKirill.StartStrategies();
+            //client.StartStrategies();
 
             Console.ReadLine();
         }
