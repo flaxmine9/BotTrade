f599b178e35419da617b9a0ca6420953f3574a0b
flaxmine9 <flaxmine9@gmail.com>
2021-10-05 11:34:59 +0300

    added a new fields

--- TechnicalIndicator/Models/Kline.cs
+++ TechnicalIndicator/Models/Kline.cs
@@ -7,5 +7,9 @@
         public decimal Open { get; set; }
         public decimal Low { get; set; }
         public decimal High { get; set; }
+        public decimal Volume { get; set; }
+        public int TradeCount { get; set; }
+        public decimal TakerBuyBaseVolume { get; set; }
+        public decimal TakerBuyQuoteVolume { get; set; }
     }
 }
