38652c15df2302e5af4452a5d52e0b99eb75c041
flaxmine9 <flaxmine9@gmail.com>
2021-10-06 22:08:25 +0300

    added a new field - PartOfBalance

--- TradeBinance/TradeSetting.cs
+++ TradeBinance/TradeSetting.cs
@@ -6,19 +6,21 @@
         public decimal StopLoss { get; set; }
         public int Leverage { get; set; }
         public int MaxOrders { get; set; }
+        public decimal PartOfBalance { get; set; }
 
         /// <summary>
         /// Types: Isolated, Cross
         /// </summary>
         public string FuturesMarginType { get; set; }
 
-        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders)
+        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal partOfBalance)
         {
             TakeProfit = takeProfit;
             StopLoss = stopLoss;
             Leverage = leverage;
             FuturesMarginType = futuresMarginType;
             MaxOrders = maxOrders;
+            PartOfBalance = partOfBalance;
         }
     }
 }
