97b0af7b19c946461e83c6e1b7b75e3a28ad9356
flaxmine9 <flaxmine9@gmail.com>
2021-10-08 22:24:35 +0300

    changed fields

--- TradeBinance/TradeSetting.cs
+++ TradeBinance/TradeSetting.cs
@@ -6,21 +6,21 @@
         public decimal StopLoss { get; set; }
         public int Leverage { get; set; }
         public int MaxOrders { get; set; }
-        public decimal PartOfBalance { get; set; }
+        public decimal BalanceUSDT { get; set; }
 
         /// <summary>
         /// Types: Isolated, Cross
         /// </summary>
         public string FuturesMarginType { get; set; }
 
-        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal partOfBalance)
+        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal balanceUSDT)
         {
             TakeProfit = takeProfit;
             StopLoss = stopLoss;
             Leverage = leverage;
             FuturesMarginType = futuresMarginType;
             MaxOrders = maxOrders;
-            PartOfBalance = partOfBalance;
+            BalanceUSDT = balanceUSDT;
         }
     }
 }
