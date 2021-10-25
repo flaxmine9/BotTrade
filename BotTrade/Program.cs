870a329f3cf2b9c7a0bf543e848694bed04768f3
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:25:26 +0300

    changed client

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -1,4 +1,5 @@
-﻿using Strategies;
+﻿using DataBase;
+using Strategies;
 using System;
 using TradeBinance;
 
@@ -8,20 +9,19 @@ namespace FlaxTrade
     {
         public static void Main(string[] args)
         {
-            //string keyTest = "581d213f387a76f1280c9072d6df0112d519dbd05752e3175feb60b80726c0a5";
-            //string secretKeyTest = "69ad2ca09dfb9f21a325e1430feb5b423c763a8aa497071ca341b01adf693df2";
+            ApplicationContext db = new ApplicationContext();
 
             string keyKirill = "YL48f5d8ziBBC1XPZ9iOD3C7zw9utoOkPYg5Tqtxpi6ktesCFwGLxeOCj3Dxvidx";
             string secretKeyKirill = "wzaRVq19ithkOQ2kLCV6iNgyneveJWUMihNP2OY8WrSMZIXR27IPgEIYfhSjqnuk";
 
-            Client clientKirill = new Client(new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill });
-            clientKirill.AddStrategy(new StrategySuperTrendSSL
+            Client client = new Client("kirill", new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill }, db);
+            client.AddStrategy(new StrategySuperTrendSSL
                 (
-                   new TradeSetting(takeProfit: 1.03m, stopLoss: 1.02m, leverage: 5, futuresMarginType: "Isolated", maxOrders: 10, balanceUSDT: 20.0m)
+                   new TradeSetting(takeProfit: 1.02m, stopLoss: 1.01m, leverage: 7, futuresMarginType: "Isolated",
+                        maxOrders: 10, balanceUSDT: 15.0m, maxPositions: 2)
                 ));
 
-            clientKirill.StartStrategies();
-
+            client.StartStrategies();
 
             Console.ReadLine();
         }
