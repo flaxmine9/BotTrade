062cd855d8ba9b7db45cfe71daa63fda9e2b30d2
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:45:49 +0300

    edited

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -11,17 +11,23 @@ namespace FlaxTrade
         {
             ApplicationContext db = new ApplicationContext();
 
-            string keyKirill = "YL48f5d8ziBBC1XPZ9iOD3C7zw9utoOkPYg5Tqtxpi6ktesCFwGLxeOCj3Dxvidx";
-            string secretKeyKirill = "wzaRVq19ithkOQ2kLCV6iNgyneveJWUMihNP2OY8WrSMZIXR27IPgEIYfhSjqnuk";
 
-            Client client = new Client("kirill", new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill }, db);
-            client.AddStrategy(new StrategySuperTrendSSL
-                (
-                   new TradeSetting(takeProfit: 1.02m, stopLoss: 1.01m, leverage: 7, futuresMarginType: "Isolated",
-                        maxOrders: 10, balanceUSDT: 15.0m, maxPositions: 2)
-                ));
+            db.Users.Add(new DataBase.Models.User() { Name = "kirill", TelegramUserId = 0 });
+            db.SaveChanges();
 
-            client.StartStrategies();
+            Console.WriteLine("Данные сохранены!");
+
+            //string keyKirill = "YL48f5d8ziBBC1XPZ9iOD3C7zw9utoOkPYg5Tqtxpi6ktesCFwGLxeOCj3Dxvidx";
+            //string secretKeyKirill = "wzaRVq19ithkOQ2kLCV6iNgyneveJWUMihNP2OY8WrSMZIXR27IPgEIYfhSjqnuk";
+
+            //Client client = new Client("kirill", new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill }, db);
+            //client.AddStrategy(new StrategySuperTrendSSL
+            //    (
+            //       new TradeSetting(takeProfit: 1.02m, stopLoss: 1.01m, leverage: 7, futuresMarginType: "Isolated",
+            //            maxOrders: 10, balanceUSDT: 15.0m, maxPositions: 2)
+            //    ));
+
+            //client.StartStrategies();
 
             Console.ReadLine();
         }
