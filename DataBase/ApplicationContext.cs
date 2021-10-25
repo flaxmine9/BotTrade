c7507ad53dfee4756d19c56af241b32aadd281f6
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:19:18 +0300

    context of database

---
+++ DataBase/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using DataBase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=binanceDB;Username=postgres;Password=1234");
+        }
+    }
+}
