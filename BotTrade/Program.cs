a8cd6604364999fd92352bfcf996e97ea6a6a8ef
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:57:37 +0300

    changed

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -13,34 +13,17 @@ namespace FlaxTrade
         {
             ApplicationContext db = new ApplicationContext();
 
+            string keyKirill = "YL48f5d8ziBBC1XPZ9iOD3C7zw9utoOkPYg5Tqtxpi6ktesCFwGLxeOCj3Dxvidx";
+            string secretKeyKirill = "wzaRVq19ithkOQ2kLCV6iNgyneveJWUMihNP2OY8WrSMZIXR27IPgEIYfhSjqnuk";
 
-            db.Users.Add(new DataBase.Models.User() { Name = "kirill", TelegramUserId = 0 });
-            db.SaveChanges();
+            Client client = new Client("kirill", new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill }, db);
+            client.AddStrategy(new StrategySuperTrendSSL
+                (
+                   new TradeSetting(takeProfit: 1.02m, stopLoss: 1.01m, leverage: 7, futuresMarginType: "Isolated",
+                        maxOrders: 10, balanceUSDT: 15.0m, maxPositions: 2)
+                ));
 
-            using (ApplicationContext db2 = new ApplicationContext())
-            {
-                // получаем объекты из бд и выводим на консоль
-                var users = db.Users.ToList();
-                Console.WriteLine("Users list:");
-                foreach (User u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name}");
-                }
-            }
-
-            Console.WriteLine("Данные сохранены!");
-
-            //string keyKirill = "YL48f5d8ziBBC1XPZ9iOD3C7zw9utoOkPYg5Tqtxpi6ktesCFwGLxeOCj3Dxvidx";
-            //string secretKeyKirill = "wzaRVq19ithkOQ2kLCV6iNgyneveJWUMihNP2OY8WrSMZIXR27IPgEIYfhSjqnuk";
-
-            //Client client = new Client("kirill", new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill }, db);
-            //client.AddStrategy(new StrategySuperTrendSSL
-            //    (
-            //       new TradeSetting(takeProfit: 1.02m, stopLoss: 1.01m, leverage: 7, futuresMarginType: "Isolated",
-            //            maxOrders: 10, balanceUSDT: 15.0m, maxPositions: 2)
-            //    ));
-
-            //client.StartStrategies();
+            client.StartStrategies();
 
             Console.ReadLine();
         }
