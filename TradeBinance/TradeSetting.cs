473475f152a0de0dac8d4203c8d7fa1946495af2
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:23:22 +0300

    added a MaxPositions

--- TradeBinance/TradeSetting.cs
+++ TradeBinance/TradeSetting.cs
@@ -7,13 +7,14 @@
         public int Leverage { get; set; }
         public int MaxOrders { get; set; }
         public decimal BalanceUSDT { get; set; }
+        public int MaxPositions { get; set; }
 
         /// <summary>
         /// Types: Isolated, Cross
         /// </summary>
         public string FuturesMarginType { get; set; }
 
-        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal balanceUSDT)
+        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal balanceUSDT, int maxPositions)
         {
             TakeProfit = takeProfit;
             StopLoss = stopLoss;
@@ -21,6 +22,7 @@
             FuturesMarginType = futuresMarginType;
             MaxOrders = maxOrders;
             BalanceUSDT = balanceUSDT;
+            MaxPositions = maxPositions;
         }
     }
 }
