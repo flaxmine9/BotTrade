904fb2f93c0b41e8478b0b6730f820c1bff6a598
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:00:08 +0300

    changed fields

--- Binance/Models/OrderInfo.cs
+++ Binance/Models/OrderInfo.cs
@@ -1,8 +1,10 @@
-﻿namespace Binance.Models
+﻿using System.Collections.Generic;
+
+namespace Binance.Models
 {
     public class OrderInfo
     {
         public int QuantityOrders { get; set; }
-        public decimal QuantityAsset { get; set; }
+        public List<decimal> QuantitiesAsset { get; set; }
     }
 }
