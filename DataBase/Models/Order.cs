eaf133be2226645146dd573954d80e4bbe31ed4f
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:18:26 +0300

    added a new models of database

---
+++ DataBase/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBase.Models
+{
+    public class Order
+    {
+        public int Id {  get; set; }
+        public int UserId { get; set; }
+        public string Symbol { get; set; }
+        public string NameStrategy {  get; set; }
+        public long OrderId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Commission { get; set; }
+        public decimal RealizedPnl { get; set; }
+        public string Side { get; set; }
+        public string PositionSide { get; set; }
+        public DateTime TradeTime { get; set; }
+    }
+}
---
+++ DataBase/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DataBase.Models
+{
+    public class User
+    {
+        public int Id {  get; set; }
+        public int TelegramUserId { get; set; }
+        public string Name {  get; set; }
+    }
+}
