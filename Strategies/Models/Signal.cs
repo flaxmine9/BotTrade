88c2bf491703ee6df72fa59cbc9ccfe8f8ad6b68
flaxmine9 <flaxmine9@gmail.com>
2021-10-25 22:21:07 +0300

    added a new model

---
+++ Strategies/Models/Signal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TradeBinance.Models;
+
+namespace Strategies.Models
+{
+    public class Signal
+    {
+        public string Symbol { get; set; }
+        public TypePosition TypePosition { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CloseTime { get;set; }
+    }
+}
