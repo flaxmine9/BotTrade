3432c3882f7d6d4b13742a60da3de9803d4f5755
flaxmine9 <flaxmine9@gmail.com>
2021-10-27 21:38:08 +0300

    added a new field - TimeFrame

--- TradeBinance/TradeSetting.cs
+++ TradeBinance/TradeSetting.cs
@@ -8,13 +8,14 @@
         public int MaxOrders { get; set; }
         public decimal BalanceUSDT { get; set; }
         public int MaxPositions { get; set; }
+        public TimeFrame TimeFrame { get; set; }
 
         /// <summary>
         /// Types: Isolated, Cross
         /// </summary>
         public string FuturesMarginType { get; set; }
 
-        public TradeSetting(decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal balanceUSDT, int maxPositions)
+        public TradeSetting(TimeFrame timeFrame, decimal takeProfit, decimal stopLoss, int leverage, string futuresMarginType, int maxOrders, decimal balanceUSDT, int maxPositions)
         {
             TakeProfit = takeProfit;
             StopLoss = stopLoss;
@@ -23,6 +24,71 @@
             MaxOrders = maxOrders;
             BalanceUSDT = balanceUSDT;
             MaxPositions = maxPositions;
+            TimeFrame = timeFrame;
         }
+    }
+
+    public enum TimeFrame
+    {
+        //
+        // Summary:
+        //     1m
+        OneMinute,
+        //
+        // Summary:
+        //     3m
+        ThreeMinutes,
+        //
+        // Summary:
+        //     5m
+        FiveMinutes,
+        //
+        // Summary:
+        //     15m
+        FifteenMinutes,
+        //
+        // Summary:
+        //     30m
+        ThirtyMinutes,
+        //
+        // Summary:
+        //     1h
+        OneHour,
+        //
+        // Summary:
+        //     2h
+        TwoHour,
+        //
+        // Summary:
+        //     4h
+        FourHour,
+        //
+        // Summary:
+        //     6h
+        SixHour,
+        //
+        // Summary:
+        //     8h
+        EightHour,
+        //
+        // Summary:
+        //     12h
+        TwelveHour,
+        //
+        // Summary:
+        //     1d
+        OneDay,
+        //
+        // Summary:
+        //     3d
+        ThreeDay,
+        //
+        // Summary:
+        //     1w
+        OneWeek,
+        //
+        // Summary:
+        //     1M
+        OneMonth
     }
 }
