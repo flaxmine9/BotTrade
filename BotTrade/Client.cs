e5992fbfe9721bf883d10835d479185371795a52
flaxmine9 <flaxmine9@gmail.com>
2021-11-03 01:19:49 +0300

    changed parametres

--- BotTrade/Client.cs
+++ BotTrade/Client.cs
@@ -3,24 +3,29 @@ using Strategy.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Test;
 
 namespace FlaxTrade
 {
     public class Client
     {
+        private NetBinance _typeNetBinance { get; set; }
         private ApiSetting _apiSetting { get; set; }
         private List<IStrategy> _strategies { get; set; }
         private ApplicationContext _dataBase { get; set; }
+
         private string NameUser { get; set; }
 
-        public Client(string nameUser, ApiSetting apiSetting, ApplicationContext dataBase)
+        public Client(string nameUser, NetBinance typeNetBinance, ApiSetting apiSetting, ApplicationContext dataBase)
         {
-            _apiSetting = apiSetting;
             NameUser = nameUser;
+            _apiSetting = apiSetting;
 
             _dataBase = dataBase;
 
             _strategies = new();
+
+            _typeNetBinance = typeNetBinance;
         }
 
         public void AddStrategy(IStrategy strategy)
@@ -39,7 +44,7 @@ namespace FlaxTrade
 
             foreach (IStrategy strategy in _strategies)
             {
-                new Task(() => strategy.Start(NameUser, _apiSetting.Key, _apiSetting.SecretKey, _dataBase)).Start();
+                new Task(() => strategy.Start(NameUser, _apiSetting.Key, _apiSetting.SecretKey, _dataBase, _typeNetBinance.Equals(NetBinance.BinanceMain) ? "MainNet" : "TestNet")).Start();
             }
         }
     }
