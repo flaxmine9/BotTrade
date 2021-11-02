d4f53cdb20c7880585af08fcf854f8c41184162d
flaxmine9 <flaxmine9@gmail.com>
2021-11-03 01:18:36 +0300

    trade signal for entry market

---
+++ TradePipeLine/TradeSignal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TradeBinance.Models;
+
+namespace TradePipeLine
+{
+    public class TradeSignal
+    {
+        public string Symbol { get; set; }
+        public TypePosition TypePosition { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CloseTime { get; set; }
+    }
+}
