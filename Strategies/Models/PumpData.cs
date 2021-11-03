21bd091885faa4454a73fb0e091a4fec098112a3
flaxmine9 <flaxmine9@gmail.com>
2021-11-03 21:16:57 +0300

    added a new data of pump

---
+++ Strategies/Models/PumpData.cs
@@ -0,0 +1,10 @@
+﻿namespace Strategies.Models
+{
+    public class PumpData
+    {
+        public string Symbol { get; set; }
+        public decimal LimitVolume { get; set; }
+        public decimal TakeProfit { get; set; }
+        public decimal StopLoss { get; set; }
+    }
+}
