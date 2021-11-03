5b8ddd28959b8250db498b55053896a7b6abbf6c
flaxmine9 <flaxmine9@gmail.com>
2021-11-03 21:23:50 +0300

    using strategy of scalping

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -10,26 +10,19 @@ namespace FlaxTrade
     {
         public static void Main(string[] args)
         {
-            string keyKirill = "YL48f5d8ziBBC1XPZ9iOD3C7zw9utoOkPYg5Tqtxpi6ktesCFwGLxeOCj3Dxvidx";
-            string secretKeyKirill = "wzaRVq19ithkOQ2kLCV6iNgyneveJWUMihNP2OY8WrSMZIXR27IPgEIYfhSjqnuk";
-
             string key = "Bam9fBnYh5ZKES83ZylyHAs3nekWi22pODywKiYjRlXhTo38XQcaJk2HzrVZJPiU";
             string secretKey = "6F2LYFGc6cgLnn5fZ3gzD2ydI0xrAKA3kpvxVjOaMYXMKtN2ukk1p4TlI2NpB8QR";
 
-            Client client = new Client("kirill", NetBinance.BinanceMain, new ApiSetting() { Key = keyKirill, SecretKey = secretKeyKirill }, new ApplicationContext());
-            client.AddStrategy(new StrategySuperTrendSSL
-                (
-                   new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.02m, stopLoss: 1.01m, leverage: 7, futuresMarginType: "Isolated",
-                        maxOrders: 10, balanceUSDT: 15.0m, maxPositions: 2)
-                ));
-
-            client.StartStrategies();
-
             Client clientFlax = new Client("flaxmine", NetBinance.BinanceMain, new ApiSetting() { Key = key, SecretKey = secretKey }, new ApplicationContext());
-            clientFlax.AddStrategy(new StrategySuperTrendSSL
+            //clientFlax.AddStrategy(new StrategySuperTrendSSL
+            //    (
+            //       new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.0085m, stopLoss: 1.005m, leverage: 10, futuresMarginType: "Isolated",
+            //            maxOrders: 10, balanceUSDT: 8.5m, maxPositions: 1)
+            //    ));
+            clientFlax.AddStrategy(new Scalping
                 (
-                   new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.0085m, stopLoss: 1.005m, leverage: 10, futuresMarginType: "Isolated",
-                        maxOrders: 10, balanceUSDT: 8.5m, maxPositions: 1)
+                    new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.025m, stopLoss: 1.02m, leverage: 5, futuresMarginType: "Isolated",
+                        maxOrders: 8, balanceUSDT: 9.0m, maxPositions: 1)
                 ));
 
             clientFlax.StartStrategies();
