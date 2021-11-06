8b69b2451391ad92b07e6d322d9a1b6e22a6d581
flaxmine9 <flaxmine9@gmail.com>
2021-11-07 02:03:44 +0300

    changed

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -21,7 +21,7 @@ namespace FlaxTrade
             //    ));
             clientFlax.AddStrategy(new Scalping
                 (
-                    new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.02m, stopLoss: 1.02m, leverage: 2, futuresMarginType: "Isolated",
+                    new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.025m, stopLoss: 1.02m, leverage: 5, futuresMarginType: "Isolated",
                         maxOrders: 8, balanceUSDT: 9.0m, maxPositions: 1)
                 ));
 
