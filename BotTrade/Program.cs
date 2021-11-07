566f2fcec0426e04109ebfada9780817b119c129
flaxmine9 <flaxmine9@gmail.com>
2021-11-08 02:35:17 +0300

    edited

--- BotTrade/Program.cs
+++ BotTrade/Program.cs
@@ -14,15 +14,10 @@ namespace FlaxTrade
             string secretKey = "6F2LYFGc6cgLnn5fZ3gzD2ydI0xrAKA3kpvxVjOaMYXMKtN2ukk1p4TlI2NpB8QR";
 
             Client clientFlax = new Client("flaxmine", NetBinance.BinanceMain, new ApiSetting() { Key = key, SecretKey = secretKey }, new ApplicationContext());
-            //clientFlax.AddStrategy(new StrategySuperTrendSSL
-            //    (
-            //       new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.0085m, stopLoss: 1.005m, leverage: 10, futuresMarginType: "Isolated",
-            //            maxOrders: 10, balanceUSDT: 8.5m, maxPositions: 1)
-            //    ));
             clientFlax.AddStrategy(new Scalping
                 (
                     new TradeSetting(TimeFrame.FiveMinutes, takeProfit: 1.015m, stopLoss: 1.01m, leverage: 5, futuresMarginType: "Isolated",
-                        maxOrders: 6, balanceUSDT: 4.5m, maxPositions: 2)
+                        maxOrders: 10, balanceUSDT: 8.0m, maxPositions: 1)
                 ));
 
             clientFlax.StartStrategies();
