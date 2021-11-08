8b34913ad46e50b9a8fca528408f6d92ea860130
flaxmine9 <flaxmine9@gmail.com>
2021-11-08 15:22:46 +0300

    keys of binance

---
+++ BotTrade/BinanceKey.cs
@@ -0,0 +1,11 @@
+﻿namespace BotTrade
+{
+    public static class BinanceKey
+    {
+        public static string Key { get; set; } = "Bam9fBnYh5ZKES83ZylyHAs3nekWi22pODywKiYjRlXhTo38XQcaJk2HzrVZJPiU";
+        public static string SecretKey { get; set; } = "6F2LYFGc6cgLnn5fZ3gzD2ydI0xrAKA3kpvxVjOaMYXMKtN2ukk1p4TlI2NpB8QR";
+
+        public static string KeyTest { get; set; } = "581d213f387a76f1280c9072d6df0112d519dbd05752e3175feb60b80726c0a5";
+        public static string SecretKeyTest { get; set; } = "69ad2ca09dfb9f21a325e1430feb5b423c763a8aa497071ca341b01adf693df2";
+    }
+}
