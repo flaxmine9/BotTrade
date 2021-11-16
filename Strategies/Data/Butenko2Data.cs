751ac87d41a6dc0e3181ada847f9ed896372413a
flaxmine9 <flaxmine9@gmail.com>
2021-11-16 03:12:49 +0300

    added a new data

---
+++ Strategies/Data/Butenko2Data.cs
@@ -0,0 +1,21 @@
+﻿namespace Strategies.Data
+{
+    public class Butenko2Data
+    {
+        public string Symbol { get; set; }
+
+        public int SMAFastPeriod { get; set; }
+        public int SMASlowPeriod { get; set; }
+
+        public int SuperTrendPeriod { get; set; }
+        public int SuperTrendMultiplier { get; set; }
+
+        public int LinearRegressionPeriod { get; set; }
+        public decimal LinearRegressionSlopeValue { get; set; }
+
+        public int RocPeriod { get; set; }
+        public int RocSmoothPeriod { get; set; }
+        public decimal RocValue { get; set; }
+
+    }
+}
