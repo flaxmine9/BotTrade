5ac18f32295b19cda723609c21d2dce21e9604bc
flaxmine9 <flaxmine9@gmail.com>
2021-11-21 01:00:16 +0300

    new strategy

---
+++ Strategies/Data/ScalpingByTrendData.cs
@@ -0,0 +1,21 @@
+﻿namespace Strategies.Data
+{
+    public class ScalpingByTrendData
+    {
+        public string Symbol { get; set; }
+
+        public int SMAFastPeriod { get; set; }
+        public int SMASlowPeriod { get; set; }
+
+        public int SuperTrendPeriod { get; set; }
+        public decimal SuperTrendMultiplier { get; set; }
+
+        public int LinearRegressionPeriod { get; set; }
+        public decimal LinearRegressionSlopeValue { get; set; }
+
+        public int RocPeriod { get; set; }
+        public int RocSmoothPeriod { get; set; }
+        public decimal RocValue { get; set; }
+
+    }
+}
