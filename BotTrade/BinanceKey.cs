ef9c10d93bc61ebb8b94e9f2baeaebddb9a7d992
flaxmine9 <flaxmine9@gmail.com>
2021-11-27 14:35:55 +0300

    keys

---
+++ BotTrade/BinanceKey.cs
@@ -0,0 +1,11 @@
+﻿namespace BotTrade
+{
+    public class BinanceKey
+    {
+        public static string Key { get; set; } = "";
+        public static string SecretKey { get; set; } = "";
+
+        public static string KeyTest { get; set; } = "";
+        public static string SecretKeyTest { get; set; } = "";
+    }
+}
